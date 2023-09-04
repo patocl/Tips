dd67b6ff4c8d81a55fcd22db5ec63f9a0413a31b
patocl <patoescl@gmail.com>
2023-09-04 12:05:15 +0200

    remove generics

--- polly.vb
+++ polly.vb
@@ -1,14 +1,20 @@
-Imports System
 Imports Polly
+Public Interface IPolicyBuilder
+    Function Build() As ISyncPolicy
+    Function HandleException(Of TException As Exception)() As IPolicyBuilder
+    Function Retry(ByVal retryCount As Integer, ByVal retryCondition As Func(Of Exception, Integer, Boolean)) As IPolicyBuilder
+    Function WaitAndRetry(ByVal retryCount As Integer, ByVal sleepDurationProvider As Func(Of Integer, TimeSpan)) As IPolicyBuilder
+End Interface
 
-Public Class RetryAndWaitPolicyBuilder(Of T)
-    Implements IPolicyBuilder(Of T)
+Imports Polly
+Public Class RetryAndWaitPolicyBuilder
+    Implements IPolicyBuilder
 
     Private retryCount As Integer = 0
     Private sleepDurationProvider As Func(Of Integer, TimeSpan)
     Private exceptionType As Type
 
-    Public Function Build() As ISyncPolicy(Of T) Implements IPolicyBuilder(Of T).Build
+    Public Function Build() As ISyncPolicy Implements IPolicyBuilder.Build
         If retryCount <= 0 Then
             Throw New InvalidOperationException("Retry count must be greater than 0.")
         End If
@@ -24,27 +30,20 @@ Public Class RetryAndWaitPolicyBuilder(Of T)
         Return Policy.Handle(exceptionType).WaitAndRetry(retryCount, sleepDurationProvider)
     End Function
 
-    Public Function HandleException(Of TException As Exception)() As IPolicyBuilder(Of T) Implements IPolicyBuilder(Of T).HandleException
+    Public Function HandleException(Of TException As Exception)() As IPolicyBuilder Implements IPolicyBuilder.HandleException
         exceptionType = GetType(TException)
         Return Me
     End Function
 
-    Public Function Retry(ByVal retryCount As Integer, ByVal retryCondition As Func(Of Exception, Integer, Boolean)) As IPolicyBuilder(Of T) Implements IPolicyBuilder(Of T).Retry
+    Public Function Retry(ByVal retryCount As Integer, ByVal retryCondition As Func(Of Exception, Integer, Boolean)) As IPolicyBuilder Implements IPolicyBuilder.Retry
         Me.retryCount = retryCount
         Return Me
     End Function
 
-    Public Function WaitAndRetry(ByVal retryCount As Integer, ByVal sleepDurationProvider As Func(Of Integer, TimeSpan)) As IPolicyBuilder(Of T) Implements IPolicyBuilder(Of T).WaitAndRetry
+    Public Function WaitAndRetry(ByVal retryCount As Integer, ByVal sleepDurationProvider As Func(Of Integer, TimeSpan)) As IPolicyBuilder Implements IPolicyBuilder.WaitAndRetry
         Me.retryCount = retryCount
         Me.sleepDurationProvider = sleepDurationProvider
         Return Me
     End Function
 End Class
-
-Public Interface IPolicyBuilder(Of T)
-    Function Build() As ISyncPolicy(Of T)
-    Function HandleException(Of TException As Exception)() As IPolicyBuilder(Of T)
-    Function Retry(ByVal retryCount As Integer, ByVal retryCondition As Func(Of Exception, Integer, Boolean)) As IPolicyBuilder(Of T)
-    Function WaitAndRetry(ByVal retryCount As Integer, ByVal sleepDurationProvider As Func(Of Integer, TimeSpan)) As IPolicyBuilder(Of T)
-End Interface
 
--- testpolly.cs
+++ testpolly.cs
@@ -8,7 +8,7 @@ public class RetryAndWaitPolicyBuilderTests
     public void Build_WithValidParameters_ReturnsPolicy()
     {
         // Arrange
-        var builder = new RetryAndWaitPolicyBuilder<string>();
+        var builder = new RetryAndWaitPolicyBuilder();
         builder.HandleException<TimeoutException>().Retry(3, (ex, attempt) => true).WaitAndRetry(3, attempt => TimeSpan.FromSeconds(1));
 
         // Act
@@ -22,8 +22,8 @@ public class RetryAndWaitPolicyBuilderTests
     public void Build_WithoutRetryCount_ThrowsException()
     {
         // Arrange
-        var builder = new RetryAndWaitPolicyBuilder<string>();
-        builder.HandleException<TimeoutException>().Retry(3, (ex, attempt) => true).WaitAndRetry(3, attempt => TimeSpan.FromSeconds(1));
+        var builder = new RetryAndWaitPolicyBuilder();
+        builder.HandleException<TimeoutException>().WaitAndRetry(3, attempt => TimeSpan.FromSeconds(1));
 
         // Act and Assert
         Assert.Throws<InvalidOperationException>(() => builder.Build());
@@ -33,8 +33,8 @@ public class RetryAndWaitPolicyBuilderTests
     public void Build_WithoutSleepDurationProvider_ThrowsException()
     {
         // Arrange
-        var builder = new RetryAndWaitPolicyBuilder<string>();
-        builder.HandleException<TimeoutException>().Retry(3, (ex, attempt) => true).WaitAndRetry(3, attempt => TimeSpan.FromSeconds(1));
+        var builder = new RetryAndWaitPolicyBuilder();
+        builder.HandleException<TimeoutException>().Retry(3, (ex, attempt) => true);
 
         // Act and Assert
         Assert.Throws<InvalidOperationException>(() => builder.Build());
@@ -44,52 +44,61 @@ public class RetryAndWaitPolicyBuilderTests
     public void Build_WithoutExceptionType_ThrowsException()
     {
         // Arrange
-        var builder = new RetryAndWaitPolicyBuilder<string>();
+        var builder = new RetryAndWaitPolicyBuilder();
         builder.Retry(3, (ex, attempt) => true).WaitAndRetry(3, attempt => TimeSpan.FromSeconds(1));
 
         // Act and Assert
         Assert.Throws<InvalidOperationException>(() => builder.Build());
     }
-    
+
     [Fact]
     public void HandleException_SetsExceptionType()
     {
         // Arrange
-        var builder = new RetryAndWaitPolicyBuilder<string>();
+        var builder = new RetryAndWaitPolicyBuilder();
 
         // Act
         builder.HandleException<TimeoutException>();
 
         // Assert
-        Assert.Equal(typeof(TimeoutException), builder.GetType().GetField("exceptionType", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance).GetValue(builder));
+        var privateField = typeof(RetryAndWaitPolicyBuilder).GetField("exceptionType", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance);
+        var exceptionType = privateField.GetValue(builder) as Type;
+
+        Assert.Equal(typeof(TimeoutException), exceptionType);
     }
 
     [Fact]
     public void Retry_SetsRetryCount()
     {
         // Arrange
-        var builder = new RetryAndWaitPolicyBuilder<string>();
+        var builder = new RetryAndWaitPolicyBuilder();
 
         // Act
         builder.Retry(3, (ex, attempt) => true);
 
         // Assert
-        Assert.Equal(3, builder.GetType().GetField("retryCount", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance).GetValue(builder));
+        var privateField = typeof(RetryAndWaitPolicyBuilder).GetField("retryCount", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance);
+        var retryCount = (int)privateField.GetValue(builder);
+
+        Assert.Equal(3, retryCount);
     }
 
     [Fact]
     public void WaitAndRetry_SetsRetryCountAndSleepDurationProvider()
     {
         // Arrange
-        var builder = new RetryAndWaitPolicyBuilder<string>();
+        var builder = new RetryAndWaitPolicyBuilder();
         var sleepDurationProvider = new Func<int, TimeSpan>(attempt => TimeSpan.FromSeconds(1));
 
         // Act
         builder.WaitAndRetry(3, sleepDurationProvider);
 
         // Assert
-        var retryCount = builder.GetType().GetField("retryCount", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance).GetValue(builder);
-        var provider = builder.GetType().GetField("sleepDurationProvider", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance).GetValue(builder);
+        var privateRetryField = typeof(RetryAndWaitPolicyBuilder).GetField("retryCount", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance);
+        var retryCount = (int)privateRetryField.GetValue(builder);
+
+        var privateSleepField = typeof(RetryAndWaitPolicyBuilder).GetField("sleepDurationProvider", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance);
+        var provider = privateSleepField.GetValue(builder) as Func<int, TimeSpan>;
 
         Assert.Equal(3, retryCount);
         Assert.Equal(sleepDurationProvider, provider);
