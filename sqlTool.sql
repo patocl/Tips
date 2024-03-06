509a2df35c0e78bc0710c986aa1bf38bf8d7c141
patocl <patoescl@gmail.com>
2024-03-06 04:08:45 +0100

    count improved

--- sqlTool.sql
+++ sqlTool.sql
@@ -3,18 +3,44 @@ DECLARE @newDatabaseName NVARCHAR(128) = 'database02'
 
 DECLARE @sql NVARCHAR(MAX) = ''
 
--- Update references in Views
-SELECT @sql = @sql +
-    '-- Update ' + CAST(COUNT(*) AS NVARCHAR(10)) + ' references in Views' + CHAR(13) +
-    'ALTER VIEW ' + QUOTENAME(OBJECT_SCHEMA_NAME(v.object_id)) + '.' + QUOTENAME(v.name) + 
-    ' AS ' + REPLACE(OBJECT_DEFINITION(v.object_id), @oldDatabaseName, @newDatabaseName) + ';' + CHAR(13)
-FROM sys.views v
-WHERE OBJECT_DEFINITION(v.object_id) LIKE '%' + @oldDatabaseName + '%'
-GROUP BY OBJECT_SCHEMA_NAME(v.object_id), v.name
+-- Count references in Views
+DECLARE @ViewCount INT
+SELECT @ViewCount = COUNT(*)
+FROM sys.views
+WHERE OBJECT_DEFINITION(object_id) LIKE '%' + @oldDatabaseName + '%'
 
--- Update references in Functions
+-- Count references in Functions
+DECLARE @FunctionCount INT
+SELECT @FunctionCount = COUNT(*)
+FROM sys.sql_modules m
+INNER JOIN sys.objects o ON m.object_id = o.object_id
+WHERE m.definition LIKE '%' + @oldDatabaseName + '%'
+
+-- Count references in Stored Procedures
+DECLARE @StoredProcedureCount INT
+SELECT @StoredProcedureCount = COUNT(*)
+FROM sys.sql_modules m
+INNER JOIN sys.objects o ON m.object_id = o.object_id
+WHERE m.definition LIKE '%' + @oldDatabaseName + '%'
+
+-- Count references in Synonyms
+DECLARE @SynonymCount INT
+SELECT @SynonymCount = COUNT(*)
+FROM sys.synonyms
+WHERE base_object_name LIKE '%' + @oldDatabaseName + '%'
+
+-- Print the total number of references to update
+PRINT '-- Total objects to update: Views(' + CAST(@ViewCount AS NVARCHAR(10)) + '), Functions(' + CAST(@FunctionCount AS NVARCHAR(10)) + '), Stored Procedures(' + CAST(@StoredProcedureCount AS NVARCHAR(10)) + '), Synonyms(' + CAST(@SynonymCount AS NVARCHAR(10)) + ')' + CHAR(13)
+
+-- Generate alteration scripts for Views
 SELECT @sql = @sql +
-    '-- Update ' + CAST(COUNT(*) AS NVARCHAR(10)) + ' references in Functions' + CHAR(13) +
+    'ALTER VIEW ' + QUOTENAME(OBJECT_SCHEMA_NAME(object_id)) + '.' + QUOTENAME(name) + 
+    ' AS ' + REPLACE(OBJECT_DEFINITION(object_id), @oldDatabaseName, @newDatabaseName) + ';' + CHAR(13)
+FROM sys.views
+WHERE OBJECT_DEFINITION(object_id) LIKE '%' + @oldDatabaseName + '%'
+
+-- Generate alteration scripts for Functions
+SELECT @sql = @sql +
     'ALTER FUNCTION ' + QUOTENAME(OBJECT_SCHEMA_NAME(m.object_id)) + '.' + QUOTENAME(o.name) + 
     '(@params)' + 
     ' RETURNS ' + REPLACE(m.definition, @oldDatabaseName, @newDatabaseName) + 
@@ -22,27 +48,22 @@ SELECT @sql = @sql +
 FROM sys.sql_modules m
 INNER JOIN sys.objects o ON m.object_id = o.object_id
 WHERE m.definition LIKE '%' + @oldDatabaseName + '%'
-GROUP BY OBJECT_SCHEMA_NAME(m.object_id), o.name
 
--- Update references in Stored Procedures
+-- Generate alteration scripts for Stored Procedures
 SELECT @sql = @sql +
-    '-- Update ' + CAST(COUNT(*) AS NVARCHAR(10)) + ' references in Stored Procedures' + CHAR(13) +
     'ALTER PROCEDURE ' + QUOTENAME(OBJECT_SCHEMA_NAME(m.object_id)) + '.' + QUOTENAME(o.name) + 
     '(@params)' + 
     ' AS BEGIN ' + REPLACE(m.definition, @oldDatabaseName, @newDatabaseName) + ' END;' + CHAR(13) + CHAR(13)
 FROM sys.sql_modules m
 INNER JOIN sys.objects o ON m.object_id = o.object_id
 WHERE m.definition LIKE '%' + @oldDatabaseName + '%'
-GROUP BY OBJECT_SCHEMA_NAME(m.object_id), o.name
 
--- Update references in Synonyms
+-- Generate alteration scripts for Synonyms
 SELECT @sql = @sql +
-    '-- Update ' + CAST(COUNT(*) AS NVARCHAR(10)) + ' references in Synonyms' + CHAR(13) +
-    'DROP SYNONYM ' + QUOTENAME(s.name) + ';' + CHAR(13) +
-    'CREATE SYNONYM ' + QUOTENAME(s.name) + ' FOR ' +
-    REPLACE(s.base_object_name, @oldDatabaseName, @newDatabaseName) + ';' + CHAR(13) + CHAR(13)
-FROM sys.synonyms s
-WHERE s.base_object_name LIKE '%' + @oldDatabaseName + '%'
-GROUP BY s.name, s.base_object_name
+    'DROP SYNONYM ' + QUOTENAME(name) + ';' + CHAR(13) +
+    'CREATE SYNONYM ' + QUOTENAME(name) + ' FOR ' +
+    REPLACE(base_object_name, @oldDatabaseName, @newDatabaseName) + ';' + CHAR(13) + CHAR(13)
+FROM sys.synonyms
+WHERE base_object_name LIKE '%' + @oldDatabaseName + '%'
 
 PRINT @sql -- This will print the generated scripts
