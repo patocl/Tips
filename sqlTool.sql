aab321110225c25a3a1c4d7c5ca1f74598f2092d
patocl <patoescl@gmail.com>
2024-03-06 03:42:54 +0100

    fix name field

--- sqlTool.sql
+++ sqlTool.sql
@@ -1,38 +1,40 @@
 DECLARE @oldDatabaseName NVARCHAR(128) = 'database01'
 DECLARE @newDatabaseName NVARCHAR(128) = 'database02'
 
-DECLARE @sql NVARCHAR(MAX)
+DECLARE @sql NVARCHAR(MAX) = ''
 
 -- Update references in Views
-SELECT @sql = ISNULL(@sql, '') +
-    'ALTER VIEW ' + QUOTENAME(OBJECT_SCHEMA_NAME(object_id)) + '.' + QUOTENAME(name) + 
-    ' AS ' + REPLACE(OBJECT_DEFINITION(object_id), @oldDatabaseName, @newDatabaseName) + ';' + CHAR(13)
-FROM sys.views
-WHERE OBJECT_DEFINITION(object_id) LIKE '%' + @oldDatabaseName + '%'
+SELECT @sql = @sql +
+    'ALTER VIEW ' + QUOTENAME(OBJECT_SCHEMA_NAME(v.object_id)) + '.' + QUOTENAME(v.name) + 
+    ' AS ' + REPLACE(OBJECT_DEFINITION(v.object_id), @oldDatabaseName, @newDatabaseName) + ';' + CHAR(13)
+FROM sys.views v
+WHERE OBJECT_DEFINITION(v.object_id) LIKE '%' + @oldDatabaseName + '%'
 
 -- Update references in Functions
-SELECT @sql = @sql + ISNULL(@sql, '') +
-    'ALTER FUNCTION ' + QUOTENAME(OBJECT_SCHEMA_NAME(object_id)) + '.' + QUOTENAME(name) + 
+SELECT @sql = @sql +
+    'ALTER FUNCTION ' + QUOTENAME(OBJECT_SCHEMA_NAME(m.object_id)) + '.' + QUOTENAME(o.name) + 
     '(@params)' + 
-    ' RETURNS ' + REPLACE(definition, @oldDatabaseName, @newDatabaseName) + 
+    ' RETURNS ' + REPLACE(m.definition, @oldDatabaseName, @newDatabaseName) + 
     ' AS BEGIN RETURN 0 END;' + CHAR(13)
-FROM sys.sql_modules
-WHERE definition LIKE '%' + @oldDatabaseName + '%'
+FROM sys.sql_modules m
+INNER JOIN sys.objects o ON m.object_id = o.object_id
+WHERE m.definition LIKE '%' + @oldDatabaseName + '%'
 
 -- Update references in Stored Procedures
-SELECT @sql = @sql + ISNULL(@sql, '') +
-    'ALTER PROCEDURE ' + QUOTENAME(OBJECT_SCHEMA_NAME(object_id)) + '.' + QUOTENAME(name) + 
+SELECT @sql = @sql +
+    'ALTER PROCEDURE ' + QUOTENAME(OBJECT_SCHEMA_NAME(m.object_id)) + '.' + QUOTENAME(o.name) + 
     '(@params)' + 
-    ' AS BEGIN ' + REPLACE(definition, @oldDatabaseName, @newDatabaseName) + ' END;' + CHAR(13)
-FROM sys.sql_modules
-WHERE definition LIKE '%' + @oldDatabaseName + '%'
+    ' AS BEGIN ' + REPLACE(m.definition, @oldDatabaseName, @newDatabaseName) + ' END;' + CHAR(13)
+FROM sys.sql_modules m
+INNER JOIN sys.objects o ON m.object_id = o.object_id
+WHERE m.definition LIKE '%' + @oldDatabaseName + '%'
 
 -- Update references in Synonyms
-SELECT @sql = @sql + ISNULL(@sql, '') +
-    'DROP SYNONYM ' + QUOTENAME(name) + ';' + CHAR(13) +
-    'CREATE SYNONYM ' + QUOTENAME(name) + ' FOR ' +
-    REPLACE(base_object_name, @oldDatabaseName, @newDatabaseName) + ';' + CHAR(13)
-FROM sys.synonyms
-WHERE base_object_name LIKE '%' + @oldDatabaseName + '%'
+SELECT @sql = @sql +
+    'DROP SYNONYM ' + QUOTENAME(s.name) + ';' + CHAR(13) +
+    'CREATE SYNONYM ' + QUOTENAME(s.name) + ' FOR ' +
+    REPLACE(s.base_object_name, @oldDatabaseName, @newDatabaseName) + ';' + CHAR(13)
+FROM sys.synonyms s
+WHERE s.base_object_name LIKE '%' + @oldDatabaseName + '%'
 
 PRINT @sql -- This will print the generated scripts
